357f72ce7f6ea33122f963a22134df9b5822392c
Martin Pruus <50612280268@tptlive.ee>
2024-10-31 10:44:31 +0200

    Updated project-prigma

--- Project-Prigma/script.ts
+++ Project-Prigma/script.ts
@@ -3,9 +3,11 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 async function main() {
-    const user = await prisma.user.create({ data : {name: "Will", email: "Will@gmail.com"}})
+    const Team1 = await prisma.Team1.create({data: {nimi: "Hello", vanus: 45, meeskond: "Skibidi"}})
+    const Team2 = await prisma.Team2.create({data: {nimi: "world", vanus: 40, messkond: "Skibidi2"}})
 
-    console.log(user) 
+    console.log(Team1) 
+    console.log(Team2)
 }
 
 main()
